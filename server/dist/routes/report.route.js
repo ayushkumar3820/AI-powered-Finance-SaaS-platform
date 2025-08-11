2088555f7974b9cac35a074479b12a8a7cce0a67
ayushkumar3820 <kumarayush382000@gamil.com>
2025-08-11 23:18:11 +0530

    folder change

--- server/dist/routes/report.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const report_controller_1 = require("../controllers/report.controller");
-const reportRoutes = (0, express_1.Router)();
-reportRoutes.get("/all", report_controller_1.getAllReportsController);
-reportRoutes.get("/generate", report_controller_1.generateReportController);
-reportRoutes.put("/update-setting", report_controller_1.updateReportSettingController);
-exports.default = reportRoutes;
